b6bea557596081aaacfe377d1489d422432d6573
vlad-aks <vladaks25@gmail.com>
2024-09-21 11:19:33 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '808d926219034cf8a254f2ae1e7da410',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Vlad AKsenov',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Vlad AKsenov',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
